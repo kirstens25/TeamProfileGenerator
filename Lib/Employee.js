542a5e2e6a1d8e5cccee0c1360ab6c6583069915
Kirsten Segavac <kirstensegavac@gmail.com>
2022-05-05 12:36:11 +0800

    Tests added for employee id, email, name

--- Lib/Employee.js
+++ Lib/Employee.js
@@ -1,25 +1,29 @@
 class Employee {
 
-constructor(id, email, name){
-    this.id = id;
-    this.email = email;
-    this.name = name;
-}
-getEmail(){
-    return this.email;
-}
+    constructor(name, id, email) {
 
-getName(){
-    return this.name;
-}
+        if(!id || !email || !name) {
+            throw Error('Cannot complete without id, name or email')
+        }
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+    getEmail() {
+        return this.email;
+    }
 
-getId(){
-    return this.id;
-}
+    getName() {
+        return this.name;
+    }
 
-getRole(){
-    return 'Employee';
-}
+    getId() {
+        return this.id;
+    }
+
+    getRole() {
+        return 'Employee';
+    }
 }
 
 module.exports = Employee;
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -9,63 +9,54 @@ describe('Testing Employee Class', () => {
 
     describe('Employee class contains requested attribute methods', () => {
 
+        const defaultId = 12;
+        const defaultName = 'Sarah';
+        const defaultEmail = 'sarah@email.com';
+
+
         // check whether we can create object from class
         test('whether we can create object from class', () => {
-            const employee = new Employee(52, 'steve', 'great@email.com')
+            const employee = new Employee(defaultName, defaultId, defaultEmail);
 
             expect(employee).toBeInstanceOf(Employee);
             expect(employee).toBeInstanceOf(Object);
-
         });
+
+        test('should fail if it didn not pass in 3 arguments', () => {
+            expect(() => new Employee()).toThrow(Error),
+                expect(() => new Employee(defaultId)).toThrow(Error),
+                expect(() => new Employee(defaultId, defaultName)).toThrow(Error),
+
+                expect(new Employee(defaultName, defaultId, defaultName)).toBeInstanceOf(Employee);
+        });
+
+
         // resulting in expected outcome
         test('if employee has entered name', () => {
-            expect(true).toBe(false);
+            const testName = "Sarah";
+            const employee = new Employee(testName);
+            expect(employee.name).toBe(testName);
+        });
+
+        test('if employee has entered id', () => {
+            const testId = "3221";
+            const employee = new Employee("name", testId);
+            expect(employee.id).toBe(testId);
 
         });
 
-        test('Employee class constructor to accept 3 args (id, name, email)', () => {
-            const employee = new Employee(52, 'steve', 'great@email.com')
+        test('if employee has entered email', () => {
+            const testEmail = "sarah@email.com";
+            const employee = new Employee("name", 21, testEmail);
+            expect(employee.email).toBe(testEmail);
 
         });
-            test('if employee has entered email', () => {
-                // set up environment
-                // define truth
 
-                // run the code
+        test('whether getName() returns inputted name', () => {
+            const testName = "Sarah";
+            const employee = new Employee(testName);
+            expect(employee.getName()).toBe(testName);
 
-                // compare the result
-                expect(true).toBe(false);
-
-            });
-
-            test('if employee has entered id', () => {
-                expect(true).toBe(false);
-
-            });
-
-            test('whether getName() returns inputted name', () => {
-
-
-            });
-            test('whether getId() returns inputted id', () => {
-
-
-            });
-
-            test('whether getEmail() returns inputted email', () => {
-
-
-            });
-
-            // resulting in unexpected outcome
-            test('whether test breaks if user does not enter any input', () => {
-                expect(true).toBe(false);
-
-            });
-
-            test('should only allow a valid email address', () => {
-                expect(true).toBe(false);
-
-            });
-        });
-    });
+        },
+        )
+    })})
